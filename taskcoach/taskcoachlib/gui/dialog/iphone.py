c74daaf27320b4c2eb6fd37c078a5ccf51952188
Hong-She Liang <sorn@taorules.com>
2024-05-12 13:40:09 +0800

    Fixed hyperlink not found

--- taskcoach/taskcoachlib/gui/dialog/iphone.py
+++ taskcoach/taskcoachlib/gui/dialog/iphone.py
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import wx
-import wx.lib.hyperlink as hl
+import wx.lib.agw.hyperlink as hl
 
 from taskcoachlib.i18n import _
 from taskcoachlib import operating_system
