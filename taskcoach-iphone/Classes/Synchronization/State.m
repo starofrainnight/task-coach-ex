8432de3dc43c449a5682033719e62d2c6cb3db42
Hong-She Liang <sorn@taorules.com>
2024-06-04 21:50:17 +0800

    Removed ios version of taskcoach
    They been moved to taskcoach-ios repository

--- taskcoach-iphone/Classes/Synchronization/State.m
+++
@@ -1,7 +0,0 @@
-//
-//  State.m
-//  TaskCoach
-//
-//  Created by Jérôme Laheurte on 24/01/09.
-//  Copyright 2009 Jérôme Laheurte. See COPYING for details.
-//
--- taskcoach-iphone/Classes/Thirdparty/Badge/readme.txt
+++
@@ -1,7 +0,0 @@
-
-Badge View, from Josh Petrie
-
-http://scientificninja.com/development/iphone-badgeview-on-google-code
-
-MIT license
-
